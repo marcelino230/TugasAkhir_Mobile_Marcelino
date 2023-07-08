a133f389d995f721f998a2c2ee32ffa160e93785
marcelino230 <63226939+marcelino230@users.noreply.github.com>
2023-07-08 22:36:37 +0700

    TugasAkhir_Mobile_Marcelino
    TugasAkhir_Mobile_Marcelino

---
+++ test/specs/mobile.automation.js
@@ -0,0 +1,36 @@
+describe('User Open Application',() => {
+  it('User can open diet meal apps ', async() => {
+      await expect($('[text="Welcome..."]')).toHaveText("Welcome...");
+      await expect($('[id="com.fghilmany.dietmealapp:id/tv_header_desc"]')).toHaveText("Isi formulir untuk membuat/menambahkan user");      
+  })
+})
+
+describe('User Submit Form Data Application',() => {
+  it('User can input field name: Marcelino', async() => {
+      await $('[text="Name"]').setValue("Marcelino");
+  })
+  it('User can input field weight: 50', async() => {
+      await $('[text="Weight"]').setValue(50);
+  })
+  it('User can input field height: 165', async() => {
+      await $('[text="Height"]').setValue(165);
+  })
+  it('User can click choose option gender: Male', async() => {
+      await $('[id="com.fghilmany.dietmealapp:id/rb_male"]').click();
+  })
+  it('User can click button "NEXT"', async() => {
+      await $('[id="com.fghilmany.dietmealapp:id/bt_next"]').click();
+      await expect($('[id="com.fghilmany.dietmealapp:id/tv_header_desc"]')).toHaveText("Isi formulir untuk membuat/menambahkan user"); 
+      await expect($('[id="com.fghilmany.dietmealapp:id/title_activity"]')).toHaveText("Aktivitas sehari-hari"); 
+  })
+})
+
+describe('User Submit Form Activity',() => {
+  it('User can choose option aktivitas sehari-hari', async() => {
+      await $('[text="Sekolah, Kuliah, Kerja Kantor, dsj."]').click();
+  })
+  it('User can click button "SELESAI"', async() => {
+      await $('[id="com.fghilmany.dietmealapp:id/bt_finish"]').click();
+      await expect($('[text="Ready to some calories today?"]')).toHaveText("Ready to some calories today?");
+  })  
+})
